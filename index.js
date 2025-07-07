833d0b65654688d5a7817cf2e0c08e8d904cd5c1
clemente <clementeahuatl@gmail.com>
2025-07-07 19:34:25 -0400

    Finsih with the home dashboard  UI

--- index.js
+++ index.js
@@ -1,5 +1,46 @@
 const name = document.getElementById("nameTag");
-const boxesGrid = document.getElementById("boxes");
+const unoccupiedBoxesGrid = document.getElementById("unocupiedBoxes");
+const occupiedBoxesGrid = document.getElementById("occupiedBoxes");
+const homeBox = document.getElementById("home");
+
+
+
+let roomsinfo = `[
+        {
+            "roomNumber":"1A",
+            "floor":"1",
+            "beds":"2",
+            "bedType":"Queen",
+            "maxCap":"4",
+            "cost":"340",
+            "status":"Clean",
+            "occupied":true
+        },
+        {
+            "roomNumber":"2A",
+            "floor":"1",
+            "beds":"4",
+            "bedType":"Queen",
+            "maxCap":"8",
+            "cost":"780",
+            "status":"Clean",
+            "occupied":true
+        },
+        {
+            "roomNumber":"3A",
+            "floor":"1",
+            "beds":"4",
+            "bedType":"Queen",
+            "maxCap":"8",
+            "cost":"780",
+            "status":"Clean",
+            "occupied":false
+        }
+    ]
+     
+
+`;
+const obj = JSON.parse(roomsinfo);
 
 function addName(input){
     name.innerText = input;
@@ -15,8 +56,8 @@ function emptyRoom(
     ){
         
     //Creating the elements
-    
     const newBox = document.createElement("div");
+    const occupiedBox = document.createElement("div");
     
     const roomNum = document.createElement("p");
     const floorNum = document.createElement("p");
@@ -37,7 +78,6 @@ function emptyRoom(
     status.innerText = `Status: ${statusInput}`;
     
     //Putting the elements that was create in the box
-    
     newBox.append(roomNum);
     newBox.append(floorNum);
     newBox.append(beds);
@@ -45,14 +85,46 @@ function emptyRoom(
     newBox.append(maxCap);
     newBox.append(cost);
     newBox.append(status);
+            
     
-    //Adding the box to the box grid
+   
     
-    boxesGrid.append(newBox);
+    let count = 0;
+    while(count<obj.length){
+        if(obj[count].occupied){
+            occupiedBoxesGrid.append(newBox);
+        }else{
+            unoccupiedBoxesGrid.append(newBox);
+        }
+        count++;
+    }
     
     //Adding styles
     newBox.className = "box";
 }
+
+function displayRooms(){
+    for(let i = 0;i<obj.length;i++){
+        emptyRoom(
+            obj[i].roomNumber,
+            obj[i].floor,
+            obj[i].beds,
+            obj[i].bedType,
+            obj[i].maxCap,
+            obj[i].cost,
+            obj[i].status
+        );
+
+    }
+}
+
+function goToRooms(){
+    homeBox.style.display = "none";
+}
+function goBackHome(){
+    homeBox.style.display = "block";
+}
+console.log(obj[0]);
 addName("Bob");
-emptyRoom("1A","1","2","Queen","4","340","Clean");
+displayRooms();
 
